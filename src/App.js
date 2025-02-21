900a6f95ca42ef5932236320a4ad0fa62407bf75
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-02-21 15:41:46 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import './App.css';
 import Greetings from './component/greeting/Greetings';
-
+import LoginProfile from './component/login/LoginProfile';
 function App() {
   console.log("rgr")
   const hobbies = ["Coding", "Reading", "Gaming", "Music"];
   return (
     <div>
-
-      <Greetings name="sawan" message="hello"  hobbies={hobbies}/>
-      
+      <Greetings name="sawan" message="hello" hobbies={hobbies}/>
+      <LoginProfile />
     </div>
   );
 }
---
+++ src/loginToProfile/LoginProfile.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function UserProfile() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    //Toggle login status for demonstration
+    const toggleLogin = () => {
+        setIsLoggedIn(!isLoggedIn);
+    };
+
+    return (
+        <div>
+            <h1>{isLoggedIn ? "Welcome back!" : "Please log in."}</h1>
+            <button onClick={toggleLogin}>
+                {isLoggedIn ? "Log out" : "Log in"}
+            </button>
+        </div>
+    );
+}
+
