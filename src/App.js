7d4d3e8f9b602975835011f990253addea42332c
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-02-22 15:41:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Greetings from './component/greeting/Greetings';
-import LoginProfile from './component/login/LoginProfile';
+import LoginProfile from './component/loginToProfile/LoginProfile';
 function App() {
   console.log("rgr")
   const hobbies = ["Coding", "Reading", "Gaming", "Music"];
