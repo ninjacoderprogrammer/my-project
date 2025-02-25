5b6655b20962b7c3efa2a42894a23a23655edecf
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-02-25 15:44:35 +0530

    changes

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -9,14 +9,11 @@ const NavBar = () => {
           <li><a href="/">Home</a></li>
           <li><a href="/about">About Us</a></li>
           <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/help">Help</a></li>
           <li><a href="/login">Login</a></li>
           <li><a href="/signup">Sign Up</a></li>
-          <li><a href="/help">Help</a></li>
         </ul>
       </nav>
-      <footer className="footer">
-        <p>&copy; MCA 2023 Batch Final Project</p>
-      </footer>
     </>
   );
 };
