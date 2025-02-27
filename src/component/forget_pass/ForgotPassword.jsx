76ba6fbf1397f7f6753af572c41854cd8886cce9
arc-Rajesh <rajeshkapil00n@gmail.com>
2025-02-27 13:10:03 +0530

    changes

---
+++ src/component/forget_pass/ForgotPassword.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ForgotPasswordCss.css";
+import { Link } from "react-router-dom";
+
+
+const ForgotPassword = () => {
+  return (
+    <div className="forgot-password-container">
+      <div className="forgot-password-box">
+        <h2>Forgot Password</h2>
+        <p>Enter your email to reset your password</p>
+        <input type="email" placeholder="Email" className="input-field" />
+        <button className="reset-button">Reset Password</button>
+        <p className="back-to-login">
+          <a href="#">Back to Login</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
