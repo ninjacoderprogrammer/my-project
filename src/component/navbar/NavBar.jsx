c524598aaebaee873ff6664672aaa9df7fbbb152
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-02-27 16:12:09 +0530

    changes

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/login">Login</Link></li>
