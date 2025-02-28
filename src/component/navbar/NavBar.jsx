c834f8bef81f3a8ffe0a2d1f7b22ec5b2e8a0c4b
arc-Rajesh <rajeshkapil00n@gmail.com>
2025-02-28 17:59:00 +0530

    nev bar added

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
 function NavBar() {
+
+  
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
-      </ul>
-    </nav>
+    <nav className="navbar">
+    <div className="nav-container">
+      <h2 className="logo">My Website</h2>
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/signup">Sign Up</Link></li>
+    </ul>
+    </div>
+  </nav>
   );
-}
+};
+
 
 export default NavBar;
