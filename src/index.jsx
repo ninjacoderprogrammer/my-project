411916e1c01d50c15d09ade2a89c0b2165e8dfa2
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-02-28 19:03:21 +0530

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,8 @@ import Contact from "./routes/Contact.jsx";
 import Login from "./routes/Login.jsx";
 import SignUp from "./routes/SignUp.jsx";
 import Home from "./routes/Home.jsx"
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,9 +16,16 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> }, // Default route
       { path: "contact", element: <Contact /> },
-      { path: "login", element: <Login /> },
+      { 
+        path: "login", 
+        element: <Login /> 
+      },
       { path: "signup", element: <SignUp /> },
-      { path: "home", element: <Home /> }
+      { path: "home", element: <Home /> },
+      { 
+        path: "dashboard", 
+        element: <ProtectedRoute><Dashboard /></ProtectedRoute> 
+      }
     ]
   }
 ]);
