0918dc6c8100c25e41b74e138faf201df890b660
sawan bhardwaj <sawan.bhardwaj.2023@gmail.com>
2025-03-04 16:02:19 +0530

    sign up

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 // import '../component/Home/Home.css';
 
-const SignupPage = () => {̥
+const SignupPage = () => {
 
     const [name, setName] = useState()
     const [email, setEmail] = useState()
@@ -19,7 +19,7 @@ const SignupPage = () => {̥
           navigate('/login')
         })
         .catch(err=> console.log(err))
-    }̥
+    }
 
   return (
     <div className="signup-container">
@@ -46,36 +46,21 @@ const SignupPage = () => {̥
             <label>Password</label>
             <input type="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
 
-            {/* <label>Website</label>
-            <input type="text" placeholder="Website" /> */}
-
-            {/* <div className="checkbox">
-              <input type="checkbox" id="terms" required />
-              <label htmlFor="terms">
-                I Agree All Statements In Terms Of Service
-              </label>
-            </div> */}
-
             <button type="submit">Create an account</button>
           </form>
 
           <div className="social-signup">
             <p>or</p>
             <p>Signup with these services</p>
-            {/* <div className="social-icons">
-              <i className="fab fa-google"></i>
-              <i className="fab fa-facebook"></i>
-              <i className="fab fa-twitter"></i>
-            </div> */}
           </div>
 
           <p className="login-link">
-            I'm already a member! <a href="#">Sign In</a>
+            I'm already a member! <a href="/"> Sign In </a>
           </p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default SignupPage;
